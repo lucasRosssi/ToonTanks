267c17f2f3621ce8f9ba59f10ffcb967b8d72b0e
lucasRosssi <lucasrm2305@gmail.com>
2022-11-27 13:09:51 -0300

    feat: create meshes and spawn point

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -13,6 +13,14 @@ ABasePawn::ABasePawn()
   CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
   RootComponent = CapsuleComp;
 
+  BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+  BaseMesh->SetupAttachment(CapsuleComp);
+
+  TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+  TurretMesh->SetupAttachment(BaseMesh);
+
+  ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn Point"));
+  ProjectileSpawnPoint->SetupAttachment(TurretMesh);
 }
 
 // Called when the game starts or when spawned
