d1c7b0f0a0a7f3ce4dacaa173fad7308b02424e8
lucasRosssi <lucasrm2305@gmail.com>
2022-11-27 18:57:20 -0300

    feat: add input and move function

--- Source/ToonTanks/Tank.cpp
+++ Source/ToonTanks/Tank.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Tank.h"
+
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 
@@ -12,4 +13,19 @@ ATank::ATank()
   
   Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
   Camera->SetupAttachment(SpringArm);
+}
+
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+  Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+  PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
+}
+
+void ATank::Move(float Value)
+{
+  FVector DeltaLocation(0.f);
+  DeltaLocation.X = Value;
+
+  AddActorLocalOffset(DeltaLocation);
 }
--- Source/ToonTanks/Tank.h
+++ Source/ToonTanks/Tank.h
@@ -17,9 +17,15 @@ class TOONTANKS_API ATank : public ABasePawn
 public:
   ATank();
 
-protected:
+  // Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
   UPROPERTY(VisibleAnywhere, Category = "Components")
   class USpringArmComponent* SpringArm;
   UPROPERTY(VisibleAnywhere, Category = "Components")
   class UCameraComponent* Camera;
+
+  void Move(float Value);
+
 };
