16a785937a767869297bcba4c7f94fe193a23f3a
lucasRosssi <lucasrm2305@gmail.com>
2022-11-27 13:10:03 -0300

    feat: add stats properties

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -16,6 +16,21 @@ public:
 	// Sets default values for this pawn's properties
 	ABasePawn();
 
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  float MovementSpeed = 400.f;
+  UPROPERTY(EditInstanceOnly, BlueprintReadWrite)
+  float TurnSpeed = 180.f;
+  UPROPERTY(EditInstanceOnly, BlueprintReadWrite)
+  float ProjectileSpeed = 1000.f;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  float FireRate = 1.f;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  float BaseDamage = 30.f;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  float Health = 100.f;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  float Shield = 50.f;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -23,6 +38,15 @@ protected:
 private:
   UPROPERTY()
   class UCapsuleComponent* CapsuleComp;
+
+  UPROPERTY()
+  UStaticMeshComponent* BaseMesh;
+
+  UPROPERTY()
+  UStaticMeshComponent* TurretMesh;
+
+  UPROPERTY()
+  USceneComponent* ProjectileSpawnPoint;
 
 public:	
 	// Called every frame
