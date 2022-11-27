b6d0546a33ab27c947cedee767778ec2f68cf6d6
lucasRosssi <lucasrm2305@gmail.com>
2022-11-27 18:57:06 -0300

    refactor: remove setup player input

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -51,7 +51,4 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 };
