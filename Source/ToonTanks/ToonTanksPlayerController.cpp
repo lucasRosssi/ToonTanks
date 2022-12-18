f8fb378144731393d6b66c43f44a547f8b224298
lucasRosssi <lucasrm2305@gmail.com>
2022-12-18 20:52:33 -0300

    feat: create player controller class

---
+++ Source/ToonTanks/ToonTanksPlayerController.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ToonTanksPlayerController.h"
+#include "GameFramework/Pawn.h"
+
+void AToonTanksPlayerController::SetPlayerEnabledState(bool bPlayerEnabled)
+{
+  if (bPlayerEnabled)
+  {
+    GetPawn()->EnableInput(this);
+  }
+  else
+  {
+    GetPawn()->DisableInput(this);
+  }
+  bShowMouseCursor = bPlayerEnabled;
+}
